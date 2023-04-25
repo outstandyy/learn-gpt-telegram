28743e057be95ed786cde71a3248ba01d8bff3e3
outstandyy <zinchenko.vladislav.1995@gmail.com>
2023-04-25 21:09:37 +0530

    init

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import { Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters';
+import config from 'config';
+
+const bot = new Telegraf(config.get('TELEGRAM_TOKEN'));
+
+bot.command('start', async (ctx) => {
+	await ctx.reply(JSON.stringify(ctx.message, null, 2));
+});
+
+bot.on(message('text'), async (ctx) => {
+	await ctx.reply(JSON.stringify(ctx.message.voice, null, 2));
+});
+
+bot.on(message('voice'), async (ctx) => {
+	try {
+		const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
+		const userId = String(ctx.message.from.id);
+		console.log(link.href);
+		await ctx.reply(JSON.stringify(link, null, 2));
+	} catch (e) {
+		console.log('Error while voice message', e.message);
+	}
+	await ctx.reply(JSON.stringify(ctx.message, null, 2));
+});
+
+
+bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+console.log('working');
